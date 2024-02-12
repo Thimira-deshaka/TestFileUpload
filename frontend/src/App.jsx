2d3779fe931095d516e18883fe038dcbb0ed520a
Thimira Deshaka <thimiradeshakashan0220@gmail.com>
2024-02-12 22:32:08 +0530

    Create frontend and pass the form data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 function App() {
   const [name, setName] =useState("");
   const [phone, setPhone] =useState("");
   const [email, setEmail] =useState("");
   const [file, setFile] =useState()
+
+  const upload = () => {
+    const formData = new FormData()
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("phone", phone);
+    formData.append("file", file);
+    axios.post('http://localhost:8000/create', formData)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch(er => console.log(er))
+  }
   
 
   return (
@@ -27,11 +41,11 @@ function App() {
           </div>
           <div className='col-12'>
             <label className='form-label'>Upload File</label>
-            <input type="text" className='form-control form-control-lg' placeholder='Enter Name' autoComplete='off'
+            <input type="file" className='form-control form-control-lg' autoComplete='off'
              onChange={(e) => setFile(e.target.files[0])}/>
           </div>
 
-          <button type='button' className='btn btn-primary btn-lg' style={{marginTop: '20px'}}>Upload</button>
+          <button type='button' className='btn btn-primary btn-lg' onClick={upload} style={{marginTop: '20px'}}>Upload</button>
         </div>
     </div>
   )
